09e014eeefb6d970631b9f04c1a3980bc3d1ef83
Steven Vandevelde <icid.asset@gmail.com>
2017-11-11 00:46:24 +0100

    Update content

--- src/App/Slides/Content.elm
+++ src/App/Slides/Content.elm
@@ -4,6 +4,7 @@ import Element exposing (..)
 import Element.Attributes exposing (..)
 import List exposing (singleton)
 import Styles exposing (..)
+import SyntaxHighlight
 import Types exposing (Msg)
 
 
@@ -45,15 +46,72 @@ content =
     --
     --
     , """
-      React
-      ~> Babel + ESLint + Webpack
+      What if I want a working app,
+      but also nice valid code?
 
-      Elm
-      ~> Nothing
+      <React>
+      Babel + ESLint
+      Webpack + PostCSS
+
+      [Elm]
+      One or two small libraries
+      for HTML & CSS
       """
         |> properText
         |> h3 H3 []
+
+    --
+    --
+    , """
+      What if I wanted to focus more on
+      the problem I'm trying to solve?
+
+      - Immutability
+      - Pure functions
+      - Types (Maybe type)
+
+      <React>
+      ?
+
+      [Elm]
+      Built-in
+      """
+        |> properText
+        |> h3 H3 []
+
+    --
+    --
+    , """
+      What if I made an important typo,
+      when would I usually find out about it?
+
+      <React>
+      During runtime
+
+      [Elm]
+      During compile time
+      """
+        |> properText
+        |> h3 H3 []
+
+    -- TODO:
+    -- Refactoring
+    -- https://twitter.com/meb_michael/status/903445435594940416
+    -- https://twitter.com/emmatcu/status/891141325814341632
+    -- https://twitter.com/billperegoy/status/803366811219664896
+    --
+    -- https://twitter.com/availle/status/821783943217344517
+    --
+    -- https://twitter.com/marick/status/804090726640472064
+    --
+    -- Optimizing for performance
+    -- Elm -> Html.lazy & keyed
+    -- React -> ShouldComponentUpdate
     ]
+
+
+
+-- ⚗️
 
 
 properText : String -> Element Styles Variations Msg
@@ -67,3 +125,22 @@ properText string =
         |> List.map List.reverse
         |> List.concat
         |> Element.paragraph Zed []
+
+
+
+-- ⚗️ / Code
+
+
+code : SyntaxHighlight.HCode -> Element Styles Variations Msg
+code hcode =
+    hcode
+        |> SyntaxHighlight.toBlockHtml Nothing
+        |> Element.html
+
+
+elmCode : String -> Element Styles Variations Msg
+elmCode string =
+    string
+        |> SyntaxHighlight.elm
+        |> Result.map code
+        |> Result.withDefault Element.empty
